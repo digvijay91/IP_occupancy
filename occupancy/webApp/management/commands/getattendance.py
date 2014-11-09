fc6127053425683b248f261fbfd248ad925512bc
occupancy <occupancy@nms-interns.in>
2014-11-09 23:55:37 +0530

    Made changes

--- occupancy/webApp/management/commands/getattendance.py
+++ occupancy/webApp/management/commands/getattendance.py
@@ -17,12 +17,12 @@ class Command(BaseCommand):
 		print __file__
 		today = date.today()
 		today = today -  relativedelta(days = 1)
-		last_date = today - relativedelta(days = today.day - 1)
+		#last_date = today - relativedelta(days = today.day - 1)
 		module_dir = os.path.dirname("/media/ayush/DATA/@yush/engineering/django/IP/IP_occupancy/occupancy/webApp/")
 		file_dir = os.path.join(module_dir,'token')
 		handle = open(file_dir,'r')
 		auth_token = handle.readline()
-		api_data_url = "https://192.168.1.40:9119/attendance?from=" + str(last_date) + "&to=" + str(today) + "&format=yyyy-mm-dd-hh24:mi:ss&token=" + auth_token
+		api_data_url = "https://192.168.1.40:9119/attendance?from=" + str(today) + "&to=" + str(today) + "&format=yyyy-mm-dd-hh24:mi:ss&token=" + auth_token
 		c = pycurl.Curl()
 		c.setopt(pycurl.URL, api_data_url)
 		c.setopt(pycurl.SSL_VERIFYPEER, 0)
